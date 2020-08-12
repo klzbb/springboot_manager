8c91cbf372b2ce3b8af3df8dfa024a8ec7783d13
konglingzhan <1397424822@qq.com>
2020-08-13 00:05:37 +0800

    拦截器

--- src/main/java/com/konglingzhan/manager/common/HttpInterceptor.java
+++ src/main/java/com/konglingzhan/manager/common/HttpInterceptor.java
@@ -1,4 +1,36 @@
 package com.konglingzhan.manager.common;
 
-public class HttpInterceptor {
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI();
+        Map param = request.getParameterMap();
+        log.info("request start,url:{},params:{}" + url + "===" + param);
+
+        return true;
+    }
+
+    // 正常处理请求之后调用
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    //
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+    }
 }
