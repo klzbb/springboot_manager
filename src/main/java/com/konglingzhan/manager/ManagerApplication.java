5e24cbe89ff3bd2b3f6ddb976ea41e4d4e2e5a60
konglingzhan_zt <1397424822@qq.com>
2020-12-31 17:55:02 +0800

    服务器代码部署更改

--- src/main/java/com/konglingzhan/manager/ManagerApplication.java
+++ src/main/java/com/konglingzhan/manager/ManagerApplication.java
@@ -3,14 +3,20 @@ package com.konglingzhan.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 
+@EnableCaching
 @SpringBootApplication
-public class ManagerApplication {
+public class ManagerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-
         SpringApplication.run(ManagerApplication.class, args);
-
     }
-}
 
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(ManagerApplication.class);
+//    }
+}
