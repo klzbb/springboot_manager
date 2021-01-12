8c9adb96a34fd0d9fa52628ae25865222d89b152
konglingzhan_zt <1397424822@qq.com>
2021-01-12 16:18:18 +0800

    test4

--- src/main/java/com/konglingzhan/manager/controller/UserController.java
+++ src/main/java/com/konglingzhan/manager/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
 
     @PostMapping("/userAll")
     public Result userAll(@Valid PageQuery pageQuery){
-        log.info("test3");
+        log.info("test4");
         PageResult<UserDto> userList = userService.userAll(pageQuery);
         return Result.success(userList);
     }
